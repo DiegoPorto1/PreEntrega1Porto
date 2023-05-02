f251fd493ab568c020b2375919e9d616dcdb0212
DiegoPorto1 <Diego566_@hotmail.com>
2023-05-02 16:06:17 -0300

    main

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import CartWidget from "../CartWidget/CartWidget"
+const NavBar = () => {
+return (
+    <nav> 
+    <h3> Ecommerce</h3>
+    <div>
+    <button>Hamburguesa</button>
+    <button>Bebida</button> 
+    <button>Cerveza</button>
+    </div>
+    <CartWidget />
+    </nav>
+)
+}
+export default NavBar
